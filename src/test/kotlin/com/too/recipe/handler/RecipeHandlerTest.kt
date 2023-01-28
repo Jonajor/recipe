97eab92e8d61d3cca534a10abcbb2aaefca44e90
Jonajor <jonathan.augusto.jorge19@gmail.com>
2023-01-27 18:36:13 -0800

    commentaries of class test

--- src/test/kotlin/com/too/recipe/handler/RecipeHandlerTest.kt
+++ src/test/kotlin/com/too/recipe/handler/RecipeHandlerTest.kt
@@ -44,68 +44,88 @@ class RecipeHandlerTest {
 
     @Test
     fun `Given a list of recipes, when the getAllRecipes function is called, then it should return a list of all recipes in the repository`() {
+        // Setting up the mock recipe service to return a list of recipeDTO when getAllRecipes() is called
         coEvery { recipeService.getAllRecipes() } returns listOf(recipeDTO)
 
+        // Executing the getAllRecipes() function and asserting that the returned status code is HttpStatus.OK
         runBlocking {
             val result = recipeHandler.getAllRecipes(mockServerRequest)
+            //assert that the returned status code is OK
             assertEquals(HttpStatus.OK, result.statusCode())
         }
     }
 
     @Test
     fun `Given a specific recipe ID, when the getRecipeById function is called, then it should return that specific recipe`() {
+        //Mock the getRecipeById method from recipeService to return recipeDTO
         coEvery { recipeService.getRecipeById(any()) } returns recipeDTO
 
+        //Create a new mockServerRequest with a path variable "id" set to 399
         mockServerRequest = MockServerRequest.builder()
             .pathVariable("id", "399")
             .body(recipeDTO)
 
         runBlocking {
+            //Call the getRecipeById method from recipeHandler
             val result = recipeHandler.getRecipeById(mockServerRequest)
+            //assert that the returned status code is OK
             assertEquals(HttpStatus.OK, result.statusCode())
         }
     }
 
     @Test
     fun `Given a new recipe, when the addRecipe function is called, then it should add the recipe to the repository and return the created recipe`() {
+        // setup the mock service to return a recipeDTO object when addRecipe is called with any parameter
         coEvery { recipeService.addRecipe(any()) } returns recipeDTO
 
+        // setup the mock server request to include a path variable of "id" with value "399" and a body containing the recipeDTO
         mockServerRequest = MockServerRequest.builder()
             .pathVariable("id", "399")
             .body(recipeDTO.toMono())
 
         runBlocking {
+            //Call the addRecipe method from recipeHandler
             val result = recipeHandler.addRecipe(mockServerRequest)
+            //assert that the returned status code is CREATED
             assertEquals(HttpStatus.CREATED, result.statusCode())
         }
     }
 
     @Test
     fun `Given an existing recipe and updated recipe details, when the updateRecipe function is called, then it should update the recipe in the repository and return the updated recipe`() {
+        // setup the mock service to return a recipeDTO object when updateRecipe is called with any parameter
         coEvery { recipeService.updateRecipe(any(), any()) } returns recipeDTO
 
+        // change ingredients attribute
         val dto = recipeDTO.copy(ingredients = listOf("shower", "mention", "both", "daylight", "corn"))
 
+        // setup the mock server request to include a path variable of "id" with value "399" and a body containing the recipeDTO
         mockServerRequest = MockServerRequest.builder()
             .pathVariable("id", "399")
             .body(dto.toMono())
 
         runBlocking {
+            //Call the updateRecipe method from recipeHandler
             val result = recipeHandler.updateRecipe(mockServerRequest)
+            //assert that the returned status code is OK
             assertEquals(HttpStatus.OK, result.statusCode())
         }
     }
 
     @Test
     fun `Given a specific recipe ID, when the deleteRecipe function is called, then it should delete the recipe from the repository`() {
+        //Mock the recipeService to return a Runs just when the deleteRecipe function is called with any parameter
         coEvery { recipeService.deleteRecipe(any()) } just Runs
 
+        //Creating a new mockServerRequest with path variable "id" set to 399
         mockServerRequest = MockServerRequest.builder()
             .pathVariable("id", "399")
             .body(recipeDTO)
 
+        //run the deleteRecipe function in a runBlocking block
         runBlocking {
             val result = recipeHandler.deleteRecipe(mockServerRequest)
+            //assert that the returned status code is NO_CONTENT
             assertEquals(HttpStatus.NO_CONTENT, result.statusCode())
         }
     }
--- src/test/kotlin/com/too/recipe/service/RecipeServiceTest.kt
+++ src/test/kotlin/com/too/recipe/service/RecipeServiceTest.kt
@@ -21,6 +21,11 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 import java.util.*
 
+/**
+ *  This class contains test cases for RecipeService
+ *  @ExtendWith(MockKExtension::class) annotation is used to enable the MockK library
+ *  @InjectMockKs and @MockK annotations are used to create mock objects
+ */
 @ExtendWith(MockKExtension::class)
 class RecipeServiceTest {
 
@@ -34,6 +39,9 @@ class RecipeServiceTest {
 
     private lateinit var recipe: Recipe
 
+    /**
+     * setup method is run before each test to initialize variables
+     */
     @BeforeEach
     fun setup() {
         recipeDTO = RecipeDTO(
@@ -52,8 +60,14 @@ class RecipeServiceTest {
         )
     }
 
+    /**
+     * Test case for addRecipe method
+     * Given a valid recipe, when the addRecipe method is called,
+     * then the recipe should be successfully added and returned
+     */
     @Test
     fun `Given a valid recipe, when the addRecipe method is called, then the recipe should be successfully added and returned`() {
+        //configuring the mock object to return the recipe when the save() method is called
         every { recipeRepository.save(any()) } returns recipe
 
         // We need to execute the method addRecipe inside CoroutineScope
@@ -69,27 +83,36 @@ class RecipeServiceTest {
     fun `Given a valid recipe ID, when the getRecipeById method is called, then the corresponding recipe should be returned`(
         input: Long
     ) {
+        // Change the value of recipe.id to storage the input value
         recipe.id = input
+        //configuring the mock object to return an optional recipe when the findById() method is called
         every { recipeRepository.findById(any()) } returns Optional.of(recipe)
 
         // We need to execute the method getRecipeById inside CoroutineScope
         runBlocking {
             val result = recipeService.getRecipeById(input)
+            //Checking only id stored in database
             assertEquals(input, result.id)
         }
     }
 
     @Test
     fun `Given a valid recipe ID, when the updateRecipe method is called, then the corresponding recipe should be successfully updated and returned`() {
+        //Creating a list of ingredients
         val ingredients = listOf("engineer", "very", "smooth", "happy", "propose")
 
+        // Mock object to return an optional recipe when the findById() method is called
         every { recipeRepository.findById(any()) } returns Optional.of(recipe)
+        // Mock object to return the recipe when the save() method is called
         every { recipeRepository.save(any()) } returns recipe.copy(ingredients = ingredients)
 
+        //Creating a copy of recipeDTO changing the ingredients
         val dto = recipeDTO.copy(ingredients = ingredients)
 
+        // Running recipeService.updateRecipe() inside CoroutineScope
         runBlocking {
             val result = recipeService.updateRecipe(451, dto)
+            // Using assertAll to do all assertions
             assertAll(Executable {
                 assertEquals("engineer", result.ingredients[0])
                 assertEquals("very", result.ingredients[1])
@@ -103,22 +126,29 @@ class RecipeServiceTest {
     @ParameterizedTest
     @ValueSource(longs = [365, 926, 143, 319, 682])
     fun `Given a valid recipe ID, when the deleteRecipe method is called, then the corresponding recipe should be successfully deleted`(input: Long) {
+        //Mocking the existsById method that will return a boolean type
         every { recipeRepository.existsById(any()) } returns true
+        //Mocking the deleteById method that only run
         every { recipeRepository.deleteById(any()) } just Runs
 
+        //Running the deleteRecipe method in a CoroutineScope
         runBlocking {
             recipeService.deleteRecipe(input)
         }
 
+        //Checking how many times the deleteById method is run
         verify(exactly = 1) { recipeRepository.deleteById(input) }
     }
 
     @Test
     fun `Given a list of recipes in the database, when the getAllRecipes method is called, then the list of all recipes should be returned`() {
+        //Mocking the findAll() method that will return a list of Recipe
         every { recipeRepository.findAll() } returns listOf(recipe)
 
+        //Running the getAllRecipes() method in a CoroutineScope
         runBlocking {
             val allRecipes = recipeService.getAllRecipes()
+            //Checking the size of list
             assertTrue(allRecipes.size == 1)
         }
     }
@@ -126,44 +156,54 @@ class RecipeServiceTest {
     @ParameterizedTest
     @ValueSource(longs = [215, 953, 264, 124, 373])
     fun `Given a non-existing recipe ID, when the getRecipeById method is called, then an exception should be thrown`(input: Long) {
-
+        //Mocking the findById() method that will return an Optional type of Recipe or Empty
         every { recipeRepository.findById(any()) } returns Optional.empty()
 
+        //Checking the exception in case of findById method return null or empty should return RecipeNotFoundException
         val notFoundException = assertThrows(RecipeNotFoundException::class.java) {
             runBlocking {
                 recipeService.getRecipeById(input)
             }
         }
 
+        //Checking the exception message
         assertEquals("Recipe with id $input not found", notFoundException.message)
     }
 
     @ParameterizedTest
     @ValueSource(longs = [915, 940, 958, 136, 881])
     fun `Given a non-existing recipe ID, when the updateRecipe method is called, then an exception should be thrown`(input: Long) {
+        //Mocking the findById() method that will return an Optional type of Recipe or Empty
         every { recipeRepository.findById(any()) } returns Optional.empty()
 
+        //Checking the exception in case of findById method return null or empty should return RecipeNotFoundException
         val notFoundException = assertThrows(RecipeNotFoundException::class.java) {
             runBlocking {
                 recipeService.updateRecipe(input, recipeDTO)
             }
         }
 
+        //Checking the exception message
         assertEquals("Recipe with id $input not found", notFoundException.message)
     }
 
     @ParameterizedTest
     @ValueSource(longs = [39, 250, 808, 109, 270])
     fun `Given a non-existing recipe ID, when the deleteRecipe method is called, then an exception should be thrown`(input: Long) {
+        //Mocking the existsById method that will return a boolean type
         every { recipeRepository.existsById(any()) } returns false
+
+        //Mocking the deleteById method that only run
         every { recipeRepository.deleteById(any()) } just Runs
 
+        //Checking the exception in case of findById method return null or empty should return RecipeNotFoundException
         val notFoundException = assertThrows(RecipeNotFoundException::class.java) {
             runBlocking {
                 recipeService.deleteRecipe(input)
             }
         }
 
+        //Checking the exception message
         assertEquals("Recipe with id $input not found", notFoundException.message)
     }
 }
